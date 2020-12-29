6af4b99c7d61a11c1d8be5f71cbb4cc000e43af5
Dmitry <agapov.box@gmail.com>
2020-12-29 19:13:04 +0300

    Write methods

--- memo.rb
+++ memo.rb
@@ -5,6 +5,26 @@ class Memo < Post
 
   def read_from_console
     #Он будет спрашивать ввод содержимого Заметки
+    puts "Все что вы введете до \"end\" попадет в заметку"
+
+    @text = []
+    line = ''
+
+    while line != "end"
+      line = STDIN.gets.chomp.encode("utf-8")
+      @text << line
+    end
+
+    @text.pop
+  end
+
+  def to_strings
+    #Метод to_strings у каждого класса должен вернуть представление содержимого данного объекта класса в вивде массива
+    # строк, готовых для записи в файл
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    return @text.unshift(time_string)
+
   end
 
 end
--- task.rb
+++ task.rb
@@ -9,10 +9,23 @@ class Task < Post
 
   def read_from_console
     #метод будет спрашивать 2 строки: описание задачи и дедлайн
+    puts "Что нужно сделать?"
+    @text = STDIN.gets.chomp.encode("utf-8")
+
+    puts "Укажите к какому числу нужно сделать задачу? Формат ДД.ММ.ГГГГ, например 10.01.2021"
+    user_input = STDIN.gets.chomp.encode("utf-8")
+    @due_date = Date.parse(user_input)
   end
 
   def to_strings
+    #Метод to_strings у каждого класса должен вернуть представление содержимого данного объекта класса в вивде массива
+    # строк, готовых для записи в файл
     # Массив из 3-х строк: делайн задачи, описание и дата задачи
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    deadline = "Крайни срок: #{@due_date}"
+
+    return [deadline, @text, time_string]
   end
 
 end
